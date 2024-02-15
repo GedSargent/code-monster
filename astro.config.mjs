c6bf1da0f04c39a68dab2cb5f629f2fa6d67b621
GedSargent <gerard.sargent@highlands.ac.uk>
2024-02-15 19:30:24 +0000

    fix: set publicPath for Github Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
+  publicPath: "/code-monster/",
   integrations: [
     starlight({
       title: "Code Monster",

08378c42f50398e3b0878143128a8d83d5d35015
GedSargent <gerard.sargent@highlands.ac.uk>
2024-04-08 22:57:26 +0100

    feat: remove toggle blur on MonsterReveal

--- src/components/react/MonsterReveal.tsx
+++ src/components/react/MonsterReveal.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import ClickOutside from "./ClickOutside";
 
 interface MonsterRevealProps {
   children: React.ReactNode;
@@ -9,27 +8,21 @@ interface MonsterRevealProps {
 export default function MonsterReveal({ children, title }: MonsterRevealProps) {
   const [isBlurred, setIsBlurred] = React.useState(true);
 
-	const handleClickOutside = () => {
-		setIsBlurred(true);
-	}
-
   const handleClickInside = () => {
-    setIsBlurred(false);
+		setIsBlurred(false);
   };
 
   return (
-		<ClickOutside onClick={handleClickOutside}>
-			<div onClick={handleClickInside} className={`relative rounded-2xl bg-white/30 border-2 border-black/10 p-8 shadow-black/40 ${isBlurred ? 'shadow-2xl' : 'shadow-md shadow-black-20'} transition duration-500 ${isBlurred ? 'cursor-pointer' : 'cursor-auto'} hover:shadow-md hover:shadow-black/20 dark:border-white/10 dark:bg-white/5`}>
-				<div className={`relative !mt-0 select-none transition duration-500 ${isBlurred ? `blur-xl select-none` : `blur-0 select-auto`}`}>
-					{children}
-				</div>
+		<div onClick={handleClickInside} className={`relative rounded-2xl bg-white/30 border-2 border-black/10 p-8 shadow-black/40 ${isBlurred ? 'shadow-2xl' : 'shadow-md shadow-black-20'} transition duration-500 ${isBlurred ? 'cursor-pointer' : 'cursor-auto'} hover:shadow-md hover:shadow-black/20 dark:border-white/10 dark:bg-white/5`}>
+			<div className={`relative !mt-0 select-none transition duration-500 ${isBlurred ? `blur-xl select-none` : `blur-0 select-auto`}`}>
+				{children}
+			</div>
 
-				<div className={`absolute inset-0 !mt-0 ${isBlurred ? 'block' : 'hidden'}`}>
-					<div className={`!mt-0 sticky top-20 text-shadow text-shadow-black/20 leading-[1.2] text-shadow-y-3 ${isBlurred ? 'text-shadow-blur-2 select-none' : 'text-shadow-transparent select-auto'} px-8 py-16 text-center text-5xl font-black text-gray-400 transition duration-500 md:text-6xl md:leading-[1.3] dark:text-gray-50 dark:text-shadow-black/70`}>
-						{title}
-					</div>
+			<div className={`absolute inset-0 !mt-0 ${isBlurred ? 'block' : 'hidden'}`}>
+				<div className={`!mt-0 sticky top-20 text-shadow text-shadow-black/20 leading-[1.2] text-shadow-y-3 ${isBlurred ? 'text-shadow-blur-2 select-none' : 'text-shadow-transparent select-auto'} px-8 py-16 text-center text-5xl font-black text-gray-400 transition duration-500 md:text-6xl md:leading-[1.3] dark:text-gray-50 dark:text-shadow-black/70`}>
+					{title}
 				</div>
 			</div>
-		</ClickOutside>
+		</div>
   );
 }

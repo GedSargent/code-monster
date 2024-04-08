077c5dfc2741e367961515ef88b294ca936f1068
GedSargent <gerard.sargent@highlands.ac.uk>
2024-04-08 17:53:30 +0100

    feat: add sticky title to MonsterReveal

--- src/components/react/MonsterReveal.tsx
+++ src/components/react/MonsterReveal.tsx
@@ -5,27 +5,26 @@ interface MonsterRevealProps {
   title: string;
 }
 
-
 export default function MonsterReveal({ children, title }: MonsterRevealProps) {
   const [isBlurred, setIsBlurred] = React.useState(true);
 
-	const handleClick = () => {
-		if (!isBlurred) return;
+  const handleClick = () => {
+    if (!isBlurred) return;
 
-		setIsBlurred(false);
-	};
+    setIsBlurred(false);
+  };
 
   return (
-		<div onClick={handleClick} className={`relative rounded-2xl border-2 border-gray-200 p-8 shadow-2xl shadow-black/40 transition duration-500 ${isBlurred ? 'cursor-pointer' : 'cursor-auto'} hover:shadow-md dark:border-gray-700 dark:bg-white/5`}>
-			<div className={`relative select-none transition duration-500 ${isBlurred ? `blur-xl select-none` : `blur-0 select-auto`}`}>
-				{children}
-			</div>
+    <div onClick={handleClick} className={`relative rounded-2xl bg-white/50 border-2 border-gray-200 p-8 shadow-2xl shadow-black/40 transition duration-500 ${isBlurred ? 'cursor-pointer' : 'cursor-auto'} hover:shadow-md dark:border-gray-700 dark:bg-white/5`}>
+      <div className={`relative !mt-0 select-none transition duration-500 ${isBlurred ? `blur-xl select-none` : `blur-0 select-auto`}`}>
+        {children}
+      </div>
 
-			<div className={`absolute inset-0 !mt-0 ${isBlurred ? 'block' : 'hidden'} `}>
-				<div className={`text-shadow text-shadow-gray-300 text-shadow-y-3 ${isBlurred ? 'text-shadow-blur-2' : 'text-shadow-transparent'} px-8 py-16 text-center text-5xl font-black text-gray-500 transition duration-500 md:text-6xl dark:text-gray-50 dark:text-shadow-black/70`}>
-					{title}
-				</div>
-			</div>
-		</div>
+      <div className={`absolute inset-0 !mt-0 ${isBlurred ? 'block' : 'hidden'}`}>
+        <div className={`!mt-0 sticky top-20 text-shadow text-shadow-black/20 leading-[1.2] text-shadow-y-3 ${isBlurred ? 'text-shadow-blur-2 select-none' : 'text-shadow-transparent select-auto'} px-8 py-16 text-center text-5xl font-black text-gray-500 transition duration-500 md:text-6xl md:leading-[1.3] dark:text-gray-50 dark:text-shadow-black/70`}>
+          {title}
+        </div>
+      </div>
+    </div>
   );
 }
